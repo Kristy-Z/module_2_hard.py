1a76885eb688dc33387f110fc875fddf8be92386
Kristy-Z <kristy38rus@gmail.com>
2024-08-29 09:35:59 +0700

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,36 @@
+import random
+
+def log():
+    numbers[3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    num = numbers(range(3,21))
+    x = random.choice(num)
+    x = log()
+    
+def password():
+    passdict = {}
+    passdict.update({3:12, 4:13, 5:1423, 6:121524, 7:162534, 8:13172635})
+    passdict.update({9:1218273645, 10:141923283746, 11:11029384756})
+    passdict.update({12:12131511124210394857, 13:112211310495867})
+    passdict.update({14:1611325212343114105968, 15:1214114232133124115106978})
+    passdict.update({16:1317115262143531341251161079, 17:11621531441351261171089})
+    passdict.update({18:12151811724272163631545414513612711810})
+    passdict.update({19:118217316415514613712811910})
+    passdict.update({20:13141911923282183731746416515614713812911})
+    password = passdict.get()
+
+def exit()
+    pair1 = list(range(3,21))
+    pair2 = list(range(3,21))
+    pairs = []
+
+
+for i in range pair1:
+    for j in pair2:
+        p1 = i
+        p2 = j
+        if p1>=p2:
+            continue
+        else:
+            a = n % p1 + p2
+            if a == 0:
+                pairs.append([p1,p2])
