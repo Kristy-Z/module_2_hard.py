702936061eb42686a04cf7c11d4fe10473217c1c
Kristy-Z <kristy38rus@gmail.com>
2024-08-31 11:34:31 +0700

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -20,7 +20,7 @@ passdict = {3:12,
 def g_code(n):
     code = ''
     for i in range(1, n):
-        for j in range(1,n):
+        for j in range(i + 1,n):
             if n % (i + j) == 0:
                 code += f'{i}{j}'
 
